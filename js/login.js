ba0f013ca2ff49d65500892291c710babd902f6f
David <771299833@qq.com>
2016-04-13 22:33:03 +0800

    Added files via upload

---
+++ js/login.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('login', []);
+app.controller('loginCtrl', function ($scope) {
+    $scope.isUserExist = true;
+    $scope.wrongPassword = false;
+    $scope.login = function (event) {
+        $scope.isUserExist = false;
+        $scope.wrongPassword = false;
+        var data = JSON.parse(localStorage.getItem('database')) || {};
+        for (var property in data) {
+            if (data.hasOwnProperty(property)) {
+                if ($scope.UserName == property) {
+                    $scope.isUserExist = true;
+                    if ($scope.Password != data[property].password) {
+                        $scope.wrongPassword = true;
+                    }
+                }
+            }
+        };
+        if ($scope.isUserExist == false || $scope.wrongPassword == true) {
+            event.preventDefault();
+        } else {
+            sessionStorage.setItem('UserName', $scope.UserName);
+        }
+
+    };
+});
